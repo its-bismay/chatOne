dcf0d462e369ad5b7bf3b0cb688f67bc2bbc6e73
its-bismay <2023ugee070@nitjsr.ac.in>
2025-06-03 16:58:50 +0530

    intial routing and components setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LogInPage from './pages/LogInPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center">
+      <Routes>
+        <Route  path='/' element={<HomePage/>}/>
+        <Route  path='/login' element={<LogInPage/>}/>
+        <Route  path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
